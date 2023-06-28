ed294648d2cd44dfc193100bafabd53cd0b8a1b2
Efrat Leibenzon <efrat.leibenzon@gmail.com>
2023-06-28 09:57:48 +0300

    first commit

---
+++ config/dev.mjs
@@ -0,0 +1,6 @@
+export default {
+  // dbURL: 'mongodb+srv://efratleibenzon:Fel666115@cluster0.fkhsuih.mongodb.net/?retryWrites=true&w=majority',
+  dbName: 'filtergram_db',
+  dbURL: 'mongodb://127.0.0.1:27017',
+  // dbName : 'tester_db'
+}
---
+++ config/prod.mjs
@@ -0,0 +1,4 @@
+export default {
+  dbURL: 'mongodb+srv://efratleibenzon:Fel666115@cluster0.fkhsuih.mongodb.net/?retryWrites=true&w=majority',
+  dbName: 'filtergram_db'
+}
